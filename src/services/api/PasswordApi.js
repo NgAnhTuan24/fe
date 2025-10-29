4982e6d0f2f74911aaaf07e588d6f91bf80e7438
Halns24 <138025120+NgAnhTuan24@users.noreply.github.com>
2025-10-29 13:15:31 +0700

    Refactor API_URL to use environment variable
    Update API_URL to use environment variable for configuration.

--- src/services/api/PasswordApi.js
+++ src/services/api/PasswordApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/auth";
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 // Gửi email khôi phục mật khẩu
 export const forgotPassword = async (email) => {
